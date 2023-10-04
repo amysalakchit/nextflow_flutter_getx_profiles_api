85b6254ca6e10dd0edf6d1eb34abd465ad37401d
Teerasej Jiraphatchandej <teerasej@gmail.com>
2023-10-04 22:17:20 +0700

    โหลดข้อมูลจาก web api และแสดงใน home page

--- lib/controllers/profile_controller.dart
+++ lib/controllers/profile_controller.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:nextflow_flutter_getx_profiles_api/models/profile_model/profile_model.dart';
 
 class ProfileController extends GetxController {
+  var profileList = <ProfileModel>[];
   var loading = true.obs;
 
   loadDataFromWebAPI() async {
@@ -9,6 +10,23 @@ class ProfileController extends GetxController {
 
     // delay 3 seconds
     await Future.delayed(Duration(seconds: 3));
+
+    var connect = Get.find<GetConnect>();
+    var response = await connect.get(
+      'https://651d740c44e393af2d59d2b4.mockapi.io/api/profiles',
+    );
+
+    var profileJSONList = response.body;
+
+    var convertedProfileList = List<ProfileModel>.from(
+      profileJSONList.map(
+        (profileJSON) {
+          return ProfileModel.fromMap(profileJSON);
+        },
+      ),
+    );
+
+    profileList = convertedProfileList;
 
     loading(false);
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:nextflow_flutter_getx_profiles_api/controllers/profile_controller.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -33,6 +34,20 @@ class HomePage extends StatelessWidget {
                     child: CircularProgressIndicator(),
                   );
                 }
+
+                return ListView.builder(
+                  itemCount: controller.profileList.length,
+                  itemBuilder: (context, index) {
+                    var profile = controller.profileList[index];
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(profile.avatar ?? ''),
+                      ),
+                      title: Text(profile.name ?? ''),
+                      subtitle: Text(profile.phone ?? ''),
+                    );
+                  },
+                );
               },
             ),
           ),
