4ff06db6ff68ce83e78ce16dd43699bf8cddcde6
amysalakchit <amysalakchit.git@gmail.com>
2024-09-19 11:30:13 +0700

    add delay to loading method

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:nextflow_flutter_getx_profiles_api/controllers/profile_controller.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //sarng ProfileController lae sai long pai nai GetX; ProfileController() ma jark profile_controller.dart
+    var controller = Get.put(ProfileController());
+
+    //ern sai method
+    controller.loadDataFromWebAPI();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Profiles'),
       ),
       // the body contains filter text field and list of profiles
-      body: ListView(),
+      body: Obx(() {
+        if (controller.loading.value == true) {
+          return Center(
+              child: CircularProgressIndicator(
+            strokeWidth: 5,
+          ));
+          return Text("loading...");
+        } else {
+          return Text('Finish.');
+        }
+      }),
     );
   }
 }
